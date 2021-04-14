7f9e8ccc0181e5c1fedcaed17224206a0d394954
maralvarig <47633216+maralvarig@users.noreply.github.com>
2021-04-14 19:57:39 +0200

    Corregidos los ID en el xml y añadido el xml de los DNI

---
+++ AccesoExcel.java
@@ -0,0 +1,281 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Row.MissingCellPolicy;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+/**
+ *
+ * @author carlos
+ */
+public class AccesoExcel {
+
+    ArrayList<String> prorrataExtra = new ArrayList<String>();
+    ArrayList<Integer> rowNull = new ArrayList<Integer>();
+    ArrayList<String> primeraFila = new ArrayList<String>();
+
+    public void acceso() throws IOException {
+        String excelFilePath = "./resources/SistemasInformacionII.xlsx";
+        FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+
+        Workbook workbook = WorkbookFactory.create(new File(excelFilePath));
+        System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
+
+        Sheet sheet = workbook.getSheetAt(0);
+
+        // Create a DataFormatter to format and get each cell's value as String
+        DataFormatter dataFormatter = new DataFormatter();
+        HashMap<String, int[]> categoria = new HashMap<String, int[]>();
+
+        // 1. You can obtain a rowIterator and columnIterator and iterate over them
+        System.out.println("\n\nIterating over Rows and Columns using Iterator\n");
+        Iterator<Row> rowIterator = sheet.rowIterator();
+        rowIterator.next();
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+
+            // Now let's iterate over the columns of the current row
+            Iterator<Cell> cellIterator = row.cellIterator();
+            ArrayList<String> col = new ArrayList<String>();
+
+//            while (cellIterator.hasNext()) {
+            int[] salarios = new int[2];
+            String cate = "";
+            for (int i = 0; i < 3; i++) {
+
+                Cell cell = cellIterator.next();
+                String cellValue = dataFormatter.formatCellValue(cell);
+                if (i == 0) {
+                    cate = cellValue;
+                } else {
+                    salarios[i - 1] = Integer.parseInt(cellValue);
+
+                }
+                System.out.print(cellValue + "\t");
+            }
+            categoria.put(cate, salarios);
+
+            for (int i = 0; i < 2; i++) {
+                Cell cell = cellIterator.next();
+                String cellValue = dataFormatter.formatCellValue(cell);
+                //  categoria.put(cellValue, new )
+                System.out.print(cellValue + "\t");
+            }
+
+//            }
+            System.out.println();
+        }
+
+        workbook.close();
+        inputStream.close();
+
+        // Create a DataFormatter to format and get each cell's value as String
+//        DataFormatter dataFormatter = new DataFormatter();
+//        ArrayList<Trabajadorbbdd> atb = new ArrayList<Trabajadorbbdd>();
+//
+//        // 1. You can obtain a rowIterator and columnIterator and iterate over them
+//        System.out.println("\n\nIterating over Rows and Columns using Iterator\n");
+//        Iterator<Row> rowIterator = sheet.rowIterator();
+//        rowIterator.next();
+//        while (rowIterator.hasNext()) {
+//            Row row = rowIterator.next();
+//
+//            // Now let's iterate over the columns of the current row
+//            Iterator<Cell> cellIterator = row.cellIterator();
+//            ArrayList<String> col = new ArrayList<String>();
+//
+//            while (cellIterator.hasNext()) {
+//                Cell cell = cellIterator.next();
+//                String cellValue = dataFormatter.formatCellValue(cell);
+//                col.add(cellValue);
+//                System.out.print(cellValue + "\t");
+//            }
+//
+//            Trabajadorbbdd tb = new Trabajadorbbdd(new Categorias(col.get(2)),);
+//            System.out.println();
+//        }
+//
+//        workbook.close();
+//        inputStream.close();
+    }
+
+    public ArrayList<Trabajadorbbdd> accesoHoja3() throws FileNotFoundException, IOException, ParseException {
+        String excelFilePath = "./resources/SistemasInformacionII.xlsx";
+        FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+        SimpleDateFormat formatter1 = new SimpleDateFormat("dd/MM/yyyy");
+
+        Workbook workbook = WorkbookFactory.create(new File(excelFilePath));
+        // workbook.setMissingCellPolicy(MissingCellPolicy.RETURN_BLANK_AS_NULL);
+
+        System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
+
+        Sheet sheet = workbook.getSheetAt(2);
+
+        // Create a DataFormatter to format and get each cell's value as String
+        DataFormatter fmt = new DataFormatter();
+
+        ArrayList<Trabajadorbbdd> atb = new ArrayList<Trabajadorbbdd>();
+        Trabajadorbbdd tb = null;
+        // 1. You can obtain a rowIterator and columnIterator and iterate over them
+        System.out.println("\n\nIterating over Rows and Columns using Iterator\n");
+
+        for (int rn = 0; rn <= sheet.getLastRowNum(); rn++) {
+            Row row = sheet.getRow(rn);
+
+            ArrayList<String> col = new ArrayList<String>();
+            
+            Categorias c = new Categorias();
+            // Row "rn" has data
+            System.out.println("-------------------------------------------------------------------------------------"+row.getLastCellNum());
+            for (int cn = 0; cn < sheet.getRow(0).getLastCellNum(); cn++) {
+                Cell cell = row.getCell(cn);
+                String cellStr = "";
+               
+                if (cell == null) {
+                    // This cell is empty/blank/un-used, handle as needed
+                    col.add(cellStr);
+                    System.out.print(cellStr + "\t");
+                } else {
+                    switch (cell.getCellType()) {
+                        case STRING:
+                                cellStr = fmt.formatCellValue(cell);
+                                col.add(cellStr);
+                                System.out.print(cellStr + "\t");
+                            break;
+                        case NUMERIC:
+                            if (DateUtil.isCellDateFormatted(cell)) {
+                               String cellValue = formatter1.format(cell.getDateCellValue());
+                               System.out.print(cellValue + "\t");
+
+                                col.add(cellValue);
+                            }
+                            break;
+                        default:
+                            cellStr = fmt.formatCellValue(cell);
+                                col.add(cellStr);
+                                System.out.print(cellStr + "\t");
+                            break;
+                    }
+                }
+            }
+            //Id de la columna
+            col.add(Integer.toString(rn));
+            
+            if(rn==0){
+                primeraFila = col;
+            }else if (col.get(0).equals("")) {
+                rowNull.add(0);
+            } else {
+                rowNull.add(1);
+
+                tb = new Trabajadorbbdd();
+                tb.setEmpresas(new Empresas(col.get(0), col.get(1)));
+
+                c.setNombreCategoria(col.get(2));
+                tb.setCategorias(c);
+
+                tb.setFechaAlta(formatter1.parse(col.get(3)));
+                System.out.println(new SimpleDateFormat("dd/MM/yyyy").parse(col.get(3)) + "--------------------------------------------------------");
+                tb.setNombre(col.get(6));
+                tb.setApellido1(col.get(4));
+                tb.setApellido2(col.get(5));
+                tb.setEmail(col.get(12));
+                tb.setNifnie(col.get(7));
+                prorrataExtra.add(col.get(8));
+                tb.setCodigoCuenta(col.get(9));
+                tb.setIban(col.get(10));
+                tb.setIdTrabajador(Integer.parseInt(col.get(13))+1);
+                atb.add(tb);
+            }
+
+            System.out.println();
+        }
+
+        workbook.close();
+        inputStream.close();
+
+        return atb;
+    }
+
+    public void cargarNuevosDatos(ArrayList<Trabajadorbbdd> atb) throws IOException {
+        Workbook workbook = new XSSFWorkbook(); // new HSSFWorkbook() for generating `.xls` file
+
+        /* CreationHelper helps us create instances of various things like DataFormat, 
+           Hyperlink, RichTextString etc, in a format (HSSF, XSSF) independent way */
+        CreationHelper createHelper = workbook.getCreationHelper();
+
+        // Create a Sheet
+        Sheet sheet = workbook.createSheet("Hoja 3");
+
+        int rowNum = 0;
+        Row row = sheet.createRow(rowNum++);
+        for(int i=0; i<13; i++){
+            row.createCell(i).setCellValue(primeraFila.get(i));
+        }
+
+
+        int x= 0;
+        for (int i = 0; i < rowNull.size(); i++) {
+            row = sheet.createRow(rowNum++);
+           
+            
+            if (rowNull.get(i) == 1) {
+                row.createCell(0).setCellValue(atb.get(i-x).getEmpresas().getNombre());
+                row.createCell(1).setCellValue(atb.get(i-x).getEmpresas().getCif());
+                row.createCell(2).setCellValue(atb.get(i-x).getCategorias().getNombreCategoria());
+                row.createCell(3).setCellValue(new SimpleDateFormat("dd/MM/yyyy").format(atb.get(i-x).getFechaAlta()));
+                //System.out.println(atb.get(i).getFechaAlta().toString());
+                row.createCell(4).setCellValue(atb.get(i-x).getApellido1());
+                row.createCell(5).setCellValue(atb.get(i-x).getApellido2());
+                row.createCell(6).setCellValue(atb.get(i-x).getNombre());
+                row.createCell(7).setCellValue(atb.get(i-x).getNifnie());
+                row.createCell(8).setCellValue(prorrataExtra.get(i-x));
+                row.createCell(9).setCellValue(atb.get(i-x).getCodigoCuenta());
+                row.createCell(10).setCellValue(atb.get(i-x).getIban().substring(0, 2));
+                row.createCell(11).setCellValue(atb.get(i-x).getIban());
+                row.createCell(12).setCellValue(atb.get(i-x).getEmail());
+            } else {
+                x++;
+            }
+
+        }
+
+        // Resize all columns to fit the content size
+        for (int i = 0; i < atb.size(); i++) {
+            sheet.autoSizeColumn(i);
+        }
+
+        // Write the output to a file
+        FileOutputStream fileOut = new FileOutputStream("./resources/Nuevo.xlsx");
+        workbook.write(fileOut);
+        fileOut.close();
+
+        // Closing the workbook
+        workbook.close();
+    }
+
+}
---
+++ SistemasInformacion2.java
@@ -0,0 +1,448 @@
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.text.ParseException;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelo.AccesoExcel;
+import modelo.Empresas;
+import modelo.EmpresasDAO;
+import modelo.HibernateUtil;
+import modelo.Nomina;
+import modelo.NominaDAO;
+import modelo.Trabajadorbbdd;
+import modelo.TrabajadorbbddDAO;
+import modelo.XMLGenerator;
+
+/*
+     * To change this license header, choose License Headers in Project Properties.
+     * To change this template file, choose Tools | Templates
+     * and open the template in the editor.
+ */ /**
+ *
+ * @author carlos
+ */
+
+public class SistemasInformacion2 {
+
+    static boolean cuentaCorrecta = false;
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException, ParseException {
+        // TODO code application logic here
+
+//        Scanner leer = new Scanner(System.in);
+//
+//       
+//        System.out.println("Introduce un DNI: ");
+//        String dni = leer.nextLine();
+//
+//        TrabajadorbbddDAO tdao = new TrabajadorbbddDAO();
+//        //Devuelve el trabajador solicitado
+//        Trabajadorbbdd tbd = tdao.buscarTrabajador(dni);
+//
+//        if (tbd == null) {
+//            System.out.print("El dni no se encuentra en la base de datos.\n");
+//            HibernateUtil.shutdown();
+//
+//        } else {
+//
+//            System.out.println("Nombre y apellidos: " + tbd.getNombre() + " " + tbd.getApellido1() + " " + tbd.getApellido2());
+//            System.out.println("NIF: " + tbd.getNifnie());
+//            System.out.println("Categoria: " + tbd.getCategorias().getNombreCategoria());
+//            System.out.println("Empresa: " + tbd.getEmpresas().getNombre());
+//
+//            int cont = 1;
+//            //obtengo todas las nominas 
+//            Iterator<Nomina> iter = tbd.getNominas().iterator();
+//            while(iter.hasNext()){
+//                Nomina nom = (Nomina) iter.next();
+//                System.out.println("Nomina" + cont + ":  " + nom.getMes() + "/" + nom.getAnio() + ",  " + nom.getBrutoNomina());
+//                cont++;
+//            }
+//            System.out.println("---------------------------------");
+//            //////////////////////////////
+//            // EJERCICIO 2
+//            //////////////////////////////
+//            //Cargo la lista de Empresas
+//            System.out.println("Nombres de las que hay que modificar:");
+//            EmpresasDAO edao = new EmpresasDAO();
+//            List<Empresas> ListaEmpresas = edao.getEmpresas(tbd.getEmpresas().getIdEmpresa());
+//            for (Empresas e : ListaEmpresas) {
+//                System.out.println(e.getNombre());
+//            }
+//            //Actualizo los datos de las empresas
+//            edao.editarEmpresas2021(ListaEmpresas);
+//            System.out.println("---------------------------------");
+//            //////////////////////////////
+//            // EJERCICIO 3
+//            //////////////////////////////
+//            NominaDAO ndao = new NominaDAO();
+//
+//            //Creo una lista con todos los idTrabajador de trabajadores que no trabajen en la empresa tbd.getEmpresas().getIdEmpresa()
+//            List<Trabajadorbbdd> ListaIdTrabajadoresToDelete = tdao.getTrabajadoresToDelete(tbd.getEmpresas().getIdEmpresa());
+//
+//            //elimino nominas asociadas a idTrabajador de la lista
+//            for (Trabajadorbbdd t : ListaIdTrabajadoresToDelete) {
+//                ndao.eliminarNominas(t.getIdTrabajador());
+//            }
+//
+//            //elimino trabajadores de la lista
+//            tdao.eliminarTrabajador(tbd.getEmpresas().getIdEmpresa());
+//
+//            HibernateUtil.shutdown();
+//        }
+        AccesoExcel ae = new AccesoExcel();
+        try {
+            // ae.acceso();
+
+            
+            
+            ArrayList<Trabajadorbbdd> atb = ae.accesoHoja3();
+            modEmail(atb);
+            modDni(atb);
+            XMLGenerator xml = new XMLGenerator("Cuentas", "ErroresCCC");
+            //Modficar codigo cuenta
+            String codigoCuenta = "";
+            String iban = "";
+            String cccErroneo = "";
+            for (int j = 0; j < atb.size(); j++) {
+                if (!atb.get(j).getNombre().equals("")) {
+                    cccErroneo = atb.get(j).getCodigoCuenta();
+                    codigoCuenta = ccc(atb.get(j).getCodigoCuenta());
+                    iban = iban(atb.get(j).getIban(), codigoCuenta);
+                    atb.get(j).setIban(iban);
+                    atb.get(j).setCodigoCuenta(codigoCuenta);
+                  
+                    if (!cuentaCorrecta) {
+                        ArrayList<String> key = new ArrayList<String>();
+                        ArrayList<String> valor = new ArrayList<String>();
+                        key.add("Nombre");
+                        key.add("Apellidos");
+                        key.add("Empresa");
+                        key.add("CCCErroneo");
+                        key.add("IBANCorrecto");
+
+                        valor.add(atb.get(j).getNombre());
+                        valor.add(atb.get(j).getApellido1() + " " + atb.get(j).getApellido2());
+                        valor.add(atb.get(j).getEmpresas().getNombre());
+                        valor.add(cccErroneo);
+                        valor.add(atb.get(j).getIban());
+
+                        xml.addItem("Cuenta", key, valor,atb.get(j).getIdTrabajador()+"");
+                    }
+
+                }
+            }
+            xml.generate();
+            
+
+            for (int i = 0; i < atb.size(); i++) {
+                Trabajadorbbdd tbd = atb.get(i);
+                System.out.println(tbd.getNombre() + ", " + tbd.getApellido1() + ", " + tbd.getApellido2() + ", " + tbd.getEmpresas().getNombre() + ", " + tbd.getEmail() + ", " + tbd.getNifnie()
+                        + ", " + tbd.getCodigoCuenta() + ", " + tbd.getIban());
+
+            }
+
+            ae.cargarNuevosDatos(atb);
+
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        }
+
+    }
+
+    private static void modDni(ArrayList<Trabajadorbbdd> atb) throws Exception {
+        char letras[] = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+        XMLGenerator xmlDNI = new XMLGenerator("Trabajadores", "Errores");
+        ArrayList<String> DNI=new ArrayList<String>();
+        for (int j = 0; j < atb.size(); j++) {
+            if (!atb.get(j).getNifnie().equals("")) {
+                String cadena = atb.get(j).getNifnie();
+                char cad[];
+                cad = cadena.toCharArray();
+                switch (cad[0]) {
+                    case 'X':
+                        cad[0] = '0';
+                        break;
+                    case 'Y':
+                        cad[0] = '1';
+                        break;
+                    case 'Z':
+                        cad[0] = '2';
+                        break;
+                }
+                String s = "";
+                for (int i = 0; i < cad.length - 1; i++) {
+                    s += cad[i];
+                }
+                int index = Integer.parseInt(s) % 23;
+                String DNIfinal=cadena;
+                if (cad[cad.length - 1] == letras[index]) {
+                    
+                } else {
+                    char cad2[] = cadena.toCharArray();
+                    cad2[cad2.length - 1] = letras[index];
+                    DNIfinal = String.valueOf(cad2);
+                    System.out.println("DNI final: " + DNIfinal);
+                    atb.get(j).setNifnie(DNIfinal);
+                }
+                if(DNI.isEmpty()){
+                    DNI.add(DNIfinal);
+                }else{
+                    boolean repetido=true;
+                    for(int i=0;i<DNI.size();i++){
+                        if (DNI.get(i).equals(DNIfinal)) {
+                            ArrayList<String> key = new ArrayList<String>();
+                            ArrayList<String> valor = new ArrayList<String>();
+                            key.add("Nombre");
+                            key.add("PrimerApellido");
+                            key.add("SegundoApellido");
+                            key.add("Empresa");
+                            key.add("Categoria");
+
+                            valor.add(atb.get(j).getNombre());
+                            valor.add(atb.get(j).getApellido1());
+                            valor.add(atb.get(j).getApellido2());
+                            valor.add(atb.get(j).getEmpresas().getNombre());
+                            valor.add(atb.get(j).getCategorias().getNombreCategoria());
+
+                            xmlDNI.addItem("Cuenta", key, valor, atb.get(j).getIdTrabajador() + "");
+                            repetido=false;
+                        }
+                    }
+                    if(repetido) DNI.add(DNIfinal);
+                }
+
+            }else{
+                ArrayList<String> key = new ArrayList<String>();
+                ArrayList<String> valor = new ArrayList<String>();
+                key.add("Nombre");
+                key.add("PrimerApellido");
+                key.add("SegundoApellido");
+                key.add("Empresa");
+                key.add("Categoria");
+
+                valor.add(atb.get(j).getNombre());
+                valor.add(atb.get(j).getApellido1());
+                valor.add(atb.get(j).getApellido2());
+                valor.add(atb.get(j).getEmpresas().getNombre());
+                valor.add(atb.get(j).getCategorias().getNombreCategoria());
+
+                xmlDNI.addItem("Cuenta", key, valor, atb.get(j).getIdTrabajador() + "");
+                
+            }
+        }
+        xmlDNI.generate();
+        xmlDNI=null;
+    }
+
+    public static String iban(String pais, String ccc) {
+        int[] valorNumerico = new int[2];
+        valorNumerico[0] = valorNumerico(pais.charAt(0));
+        valorNumerico[1] = valorNumerico(pais.charAt(1));
+
+        String cadena = ccc + valorNumerico[0] + valorNumerico[1] + "00";
+        BigInteger cadenaAux = new BigInteger(cadena);
+
+        BigInteger resto;
+        resto = cadenaAux.remainder(new BigInteger("97"));
+
+        int digito = 98 - resto.intValue();
+
+        if (digito < 10) {
+            return pais + "0" + String.valueOf(digito) + ccc;
+        } else {
+            return pais + String.valueOf(digito) + ccc;
+        }
+    }
+
+    public static String ccc(String ccc) {
+
+        int[] entidadOficina = new int[8];
+        int[] dControl = new int[2];
+        int[] id = new int[10];
+        int suma = 0;
+
+        //relleno los arrays para operar con ellos
+        for (int i = 0; i < entidadOficina.length; i++) {
+            entidadOficina[i] = Character.getNumericValue(ccc.charAt(i));
+        }
+        for (int i = 0; i < dControl.length; i++) {
+            dControl[i] = Character.getNumericValue(ccc.charAt(i + entidadOficina.length));
+        }
+        for (int i = 0; i < id.length; i++) {
+            id[i] = Character.getNumericValue(ccc.charAt(i + entidadOficina.length + dControl.length));
+        }
+
+        //calculo el digito 1
+        int[] dControlAux = new int[2];
+        String digitoControl = "00";
+        for (int i = 0; i < entidadOficina.length; i++) {
+            digitoControl += entidadOficina[i];
+        }
+        dControlAux[0] = calculadoraCCC(digitoControl);
+
+        //calculo el digito 2
+        digitoControl = "";
+        for (int i = 0; i < id.length; i++) {
+            digitoControl += id[i];
+        }
+        dControlAux[1] = calculadoraCCC(digitoControl);
+
+        //creo el ccc auxiliar
+        String cccAux = "";
+        for (int i = 0; i < entidadOficina.length; i++) {
+            cccAux += entidadOficina[i];
+        }
+        for (int i = 0; i < dControlAux.length; i++) {
+            cccAux += dControlAux[i];
+        }
+        for (int i = 0; i < id.length; i++) {
+            cccAux += id[i];
+        }
+
+        //Devuelvo el ccc correcto
+        if (dControl[0] == dControlAux[0] && dControl[1] == dControlAux[1]) {
+            System.err.println("hahdfaksdf");
+            cuentaCorrecta = true;
+            return ccc;
+        } else {
+            cuentaCorrecta = false;
+            return cccAux;
+        }
+    }
+
+    public static int calculadoraCCC(String digitoControl) {
+        int[] factores = {1, 2, 4, 8, 5, 10, 9, 7, 3, 6};
+        int suma = 0;
+        for (int i = 0; i < factores.length; i++) {
+            suma += Character.getNumericValue(digitoControl.charAt(i)) * factores[i];
+        }
+
+        int resto = suma % 11;
+        int digito = 11 - resto;
+
+        switch (digito) {
+            case 11:
+                return 0;
+            case 10:
+                return 1;
+            default:
+                return digito;
+        }
+    }
+
+    public static int valorNumerico(char i) {
+        switch (i) {
+            case 'A':
+                return 10;
+            case 'B':
+                return 11;
+            case 'C':
+                return 12;
+            case 'D':
+                return 13;
+            case 'E':
+                return 14;
+            case 'F':
+                return 15;
+            case 'G':
+                return 16;
+            case 'H':
+                return 17;
+            case 'I':
+                return 18;
+            case 'J':
+                return 19;
+            case 'K':
+                return 20;
+            case 'L':
+                return 21;
+            case 'M':
+                return 22;
+            case 'N':
+                return 23;
+            case 'O':
+                return 24;
+            case 'P':
+                return 25;
+            case 'Q':
+                return 26;
+            case 'R':
+                return 27;
+            case 'S':
+                return 28;
+            case 'T':
+                return 29;
+            case 'U':
+                return 30;
+            case 'V':
+                return 31;
+            case 'W':
+                return 32;
+            case 'X':
+                return 33;
+            case 'Y':
+                return 34;
+            case 'Z':
+                return 35;
+            default:
+                return 0;
+        }
+    }
+
+    private static void modEmail(ArrayList<Trabajadorbbdd> atb) {
+
+        ArrayList<String> emailsGenerados = new ArrayList<String>();
+
+        for (int i = 0; i < atb.size(); i++) {
+            String email = "";
+            boolean incrmentado = false;
+            Trabajadorbbdd tbd = atb.get(i);
+
+            if (!tbd.getNombre().equals("")) {
+                email = tbd.getNombre().substring(0, 1) + tbd.getApellido1().substring(0, 1);
+                if (tbd.getApellido2() != "") {
+                    email += tbd.getApellido2().substring(0, 1);
+                }
+
+                int cont = 0;
+                while (cont < emailsGenerados.size()) {
+                    if (emailsGenerados.get(cont).substring(0, 3).equals(email)) {
+                        int num = Integer.parseInt(emailsGenerados.get(cont).substring(3, 5));
+                        num++;
+                        incrmentado = true;
+                        if (num < 10) {
+                            email += "0" + String.valueOf(num);
+                        } else {
+                            email += String.valueOf(num);
+                        }
+
+                    }
+
+                    cont++;
+                }
+
+                if (!incrmentado) {
+                    email += "00";
+                }
+
+                email += "@";
+                email += tbd.getEmpresas().getNombre();
+                email += ".com";
+                tbd.setEmail(email);
+                emailsGenerados.add(email);
+            }
+
+        }
+    }
+
+}
